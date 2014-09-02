4760438df076ff4a9170aaf8598a4355eafed6a5
seancrowe5 <Sean Crowe>
2014-09-02 10:58:38 -0500

    Initial Commit

---
+++ DYO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DYO
+//
+//  Created by Sean Crowe on 9/2/14.
+//  Copyright (c) 2014 Sean Crowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DYOTests/DYOTests.m
@@ -0,0 +1,34 @@
+//
+//  DYOTests.m
+//  DYOTests
+//
+//  Created by Sean Crowe on 9/2/14.
+//  Copyright (c) 2014 Sean Crowe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DYOTests : XCTestCase
+
+@end
+
+@implementation DYOTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
