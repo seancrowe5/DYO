fe3d6f3e0f10f9ffb7c415a0f1b78006044fa4fb
seancrowe5 <Sean Crowe>
2014-09-11 18:55:26 -0500

    class for terms

---
+++ DYO/TermsandConditionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TermsandConditionsViewController.h
+//  DYO
+//
+//  Created by Sean Crowe on 9/11/14.
+//  Copyright (c) 2014 Sean Crowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermsandConditionsViewController : UIViewController
+
+@end
---
+++ DYO/TermsandConditionsViewController.m
@@ -0,0 +1,26 @@
+//
+//  TermsandConditionsViewController.m
+//  DYO
+//
+//  Created by Sean Crowe on 9/11/14.
+//  Copyright (c) 2014 Sean Crowe. All rights reserved.
+//
+
+#import "TermsandConditionsViewController.h"
+
+@interface TermsandConditionsViewController ()
+
+@end
+
+@implementation TermsandConditionsViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    //thomas add here
+    
+
+}
+
+@end
